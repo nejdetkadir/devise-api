a8dc9e2d567ea0f5b114655c0b3736175efd69af
codescaptain <codescaptain@gmail.com>
2023-01-15 01:15:12 +0300

    [update] spec/supports/dependencies/factory_bot.rb

--- spec/supports/dependencies/factory_bot.rb
+++ spec/supports/dependencies/factory_bot.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require 'factory_bot'
-FactoryBot.find_definitions
-
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryBot.find_definitions
+  end
 end
