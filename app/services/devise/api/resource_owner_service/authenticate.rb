bb8057994632c93db74ab5ba006e5b81f56479b3
Davide Abrigo <d.abrigo@bbbell.it>
2024-04-03 15:12:15 +0200

    Use authentication keys for resource lookup

--- app/services/devise/api/resource_owner_service/authenticate.rb
+++ app/services/devise/api/resource_owner_service/authenticate.rb
@@ -8,7 +8,7 @@ module Devise
         option :resource_class, type: Types::Class
 
         def call
-          resource = resource_class.find_for_authentication(email: params[:email])
+          resource = resource_class.find_for_authentication(params.slice(*resource_class.authentication_keys))
           return Failure(error: :invalid_email, record: nil) if resource.blank?
           return Failure(error: :invalid_authentication, record: resource) unless authenticate!(resource)
 
